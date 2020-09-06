a5dee023397c24ac8d9d22c6294a362fbf74d6c2
dochoex@gmail.com <abiy.terefe@hotmail.com>
2020-09-06 20:15:30 +0200

    page speed 2

--- production/index7.php
+++ production/index7.php
@@ -212,16 +212,11 @@
   <script src="../vendors/jquery/dist/jquery.min.js"></script>
   <!-- Bootstrap -->
   <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <!-- FastClick -->
-  <script src="../vendors/fastclick/lib/fastclick.js"></script>
-  <!-- NProgress -->
-  <script src="../vendors/nprogress/nprogress.js"></script>
-  <!-- iCheck -->
-  <script src="../vendors/iCheck/icheck.min.js"></script>
+
   <!-- Datatables -->
   <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
   <script src="../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-  <script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+  <!-- <script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
   <script src="../vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
   <script src="../vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
   <script src="../vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
@@ -232,7 +227,7 @@
   <script src="../vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
   <script src="../vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
   <script src="../vendors/jszip/dist/jszip.min.js"></script>
-  <script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
+  <script src="../vendors/pdfmake/build/pdfmake.min.js"></script> -->
   <!-- <script src="../vendors/pdfmake/build/vfs_fonts.js"></script> -->
 
   <!-- Custom Theme Scripts -->
