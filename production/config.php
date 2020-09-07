6603e58851c85ba4f1936bd7a2b3da543315235b
dochoex@gmail.com <abiy.terefe@hotmail.com>
2020-09-07 19:40:24 +0200

    cdn updates

--- production/config.php
+++ production/config.php
@@ -1,9 +1,8 @@
 <?php
-function headerLinks($title = null,$dir=null)
+function headerLinks($title = null, $dir = null)
 {
-    if($dir == null)
-    {
-        $dir="..";
+    if ($dir == null) {
+        $dir = "..";
     }
     echo '
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -11,15 +10,19 @@ function headerLinks($title = null,$dir=null)
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="'. $dir .'/images/flag.ico">
-    <title>' . $title . '</title>
+    <link rel="shortcut icon" href="' . $dir . '/images/flag.ico">
+    <title>' . $title . '</title>';
 
-    <!-- Bootstrap -->
-    <link href="'. $dir .'/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="'. $dir .'/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    if ($_SERVER['DOCUMENT_ROOT'] == "medic-ethiopia.com") {
+        echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">';
+    }else{
+        echo '<!-- Bootstrap -->
+        <link href="' . $dir . '/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">';
+    }
+    echo '<!-- Font Awesome -->
+    <link href="' . $dir . '/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- Custom Theme Style -->
-    <link href="'. $dir .'/build/css/custom.min.css" rel="stylesheet">';
+    <link href="' . $dir . '/build/css/custom.min.css" rel="stylesheet">';
 
     // echo '    <!-- Bootstrap -->
     //     <link href="'. $dir .'/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -117,7 +120,15 @@ function main_container_top_navigation()
 }
 function include_js()
 {
-    echo '
+    if ($_SERVER['DOCUMENT_ROOT'] == "medic-ethiopia.com") {
+        echo '
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    ';
+    } else {
+        echo '
         <script src="../vendors/jquery/dist/jquery.min.js"></script>
         <!-- Bootstrap -->
         <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
@@ -125,6 +136,7 @@ function include_js()
         <script src="../vendors/parsleyjs/dist/parsley.min.js"></script>
         <!-- Custom Theme Scripts -->
         <script src="../build/js/custom.min.js"></script>';
+    }
     /*
                 <!-- FastClick -->
         <script src="../vendors/fastclick/lib/fastclick.js"></script>
@@ -276,97 +288,98 @@ $medication = [
 
 
 $hospital =
-[
-   0 => "Choose your hospital",
-   1036 => "Addis Ababa - Zeweditu Hospital",
-1000 => "Addis Ababa - Addis Ababa Fistula Hospital",
-1001 => "Addis Ababa - ALERT",
-1002 => "Addis Ababa - Bethel Teaching General Hospital",
-1003 => "Addis Ababa - Amin General Hospital",
-1004 => "Addis Ababa - Addis General Hospital [2]",
-1005 => "Addis Ababa - Addis Hiwot Plc",
-1006 => "Addis Ababa - Anania Mothers and Children Specialized Medical Center",
-1007 => "Addis Ababa - Armed Forces General Hospital",
-1008 => "Addis Ababa - Bella Defense Referral Hospital",
-1009 => "Addis Ababa - Besegah Mother and Child Health Hospitals",
-1010 => "Addis Ababa - Brass Mother and Child Health Hospital",
-1011 => "Addis Ababa - Dagmawi Minilik Hospital",
-1012 => "Addis Ababa - DBalch Hospital",
-1013 => "Addis Ababa - Dinberua Hospital",
-1014 => "Addis Ababa - Ethio Tebib Hospital",
-1015 => "Addis Ababa - Federal Police Referral Hospital",
-1016 => "Addis Ababa - Girum General Hospital",
-1017 => "Addis Ababa - Hawassa Alatyon Hospital",
-1018 => "Addis Ababa - Hayat Hospital",
-1019 => "Addis Ababa - ICMC General Hospital CMC",
-1020 => "Addis Ababa - Kadisco General Hospital [3]",
-1021 => "Addis Ababa - Landmark General Hospital",
-1022 => "Addis Ababa - MyungSung Christian Medical Centre/Korean Hospital/MCM General Hospital",
-1023 => "Addis Ababa - Nordic Medical Center",
-1024 => "Addis Ababa - Novocare American Clinic [4]",
-1025 => "Addis Ababa - Ras Desta Damitew Hospital",
-1026 => "Addis Ababa - Saint Gabriel General Hospital[5]",
-1027 => "Addis Ababa - Shedeho Higher Clinic Dr Belaynew Mogess",
-1028 => "Addis Ababa - St. Paulos Hospital",
-1029 => "Addis Ababa - St. Yared General Hospital",
-1030 => "Addis Ababa - Tibebu Hospital",
-1031 => "Addis Ababa - Tikur Anbesa Specialized Hospital(TASH)",
-1032 => "Addis Ababa - Tirunesh Beijing General Hospital",
-1033 => "Addis Ababa - Tzna General Hospital",
-1034 => "Addis Ababa - Yearrer hospital",
-1035 => "Addis Ababa - Zenbaba General Hospital",
-1037 => "Adama - Sr. Aklesia Memorial Hospital",
-1038 => "Adama - Adama General Hospital & Medical College",
-1039 => "Adama - Haile Mariam Hospital & Medical College",
-1040 => "Bahir Dar - Bahir Dar Mini-fistula Hospital",
-1041 => "Bahir Dar - Felge Hiwot Hospital",
-1042 => "Bahir Dar - Gamby Teaching Hospital Clinic",
-1043 => "Bahir Dar - Universal Clinic",
-1044 => "Bahir Dar - Adinas Clinic",
-1045 => "Desse - Boru Meda Hospital",
-1046 => "Desse - Dessie Hospital",
-1047 => "Desse - selam hospital",
-1048 => "Desse - ethio hospital",
-1049 => "Desse - bate hospital",
-1050 => "Desse - Tossa Hospital",
-1051 => "Gondar - University of Gondar Hospital",
-1052 => "Gondar - Ibex Hospital",
-1053 => "Gondar - Harar",
-1054 => "Gondar - Harar General Hospital",
-1055 => "Gondar - Hiwot fana specialized University hospital",
-1056 => "Hawassa - Adare Hospital",
-1057 => "Hawassa - Alatiyon Hospital",
-1058 => "Hawassa - Asher Primary Hospital",
-1059 => "Hawassa - Awassa Referral Hospital",
-1060 => "Hawassa - Bete Abrham Primary Hospital",
-1061 => "Hawassa - Kibru Primary Hospital",
-1062 => "Hawassa - Yanet Internal Medicine Specialized Center",
-1063 => "Somali Region - Jig-jiga University Referral Hospital",
-1064 => "Somali Region - Karamara Hospital",
-1065 => "Somali Region - Dagahbour General Hospital",
-1066 => "Somali Region - Gode General Hospital",
-1067 => "Somali Region - Qabri Dahare General Hospital",
-1068 => "Somali Region - Warder General Hospital",
-1069 => "Somali Region - Filtu General Hospital",
-1070 => "Somali Region - Hargelle General Hospital",
-1071 => "Somali Region - Sitti General Hospital",
-1072 => "Tigray - Adigrat Hospital",
-1073 => "Tigray - Ayder Referral Hospital",
-1074 => "Tigray - Lemelem Karl Hospital",
-1075 => "Tigray - Mekelle Hospital",
-1076 => "Tigray - St. Mary Hospital",
-1077 => "Tigray - Quiha Hospital",
-1078 => "Tigray - Sihul Hospital",
-1079 => "Tigray - Abiadi Hospital",
-1080 => "Tigray - Wukro Hospital",
-1081 => "Tigray - korem Hospital",
-1082 => "Tigray - Mearig Hospital/dansha",
-1083 => "Tigray - Humera Hospital",
-1084 => "Tigray - Alamata Hospital",
-1085 => "Tigray - Adwa Hospital",
-1086 => "Tigray - Aksum University Referral Hospital",
-1087 => "Wolayta - Dubbo Catholic Hospital",
-1088 => "Wolayta - Soddo Christhian Hospital",
-1089 => "Wolayta - Soddo General Hospital",
-1090 => "Wolayta - Soddo University Hospital",
-9999 => "Other"];
+    [
+        0 => "Choose your hospital",
+        1036 => "Addis Ababa - Zeweditu Hospital",
+        1000 => "Addis Ababa - Addis Ababa Fistula Hospital",
+        1001 => "Addis Ababa - ALERT",
+        1002 => "Addis Ababa - Bethel Teaching General Hospital",
+        1003 => "Addis Ababa - Amin General Hospital",
+        1004 => "Addis Ababa - Addis General Hospital [2]",
+        1005 => "Addis Ababa - Addis Hiwot Plc",
+        1006 => "Addis Ababa - Anania Mothers and Children Specialized Medical Center",
+        1007 => "Addis Ababa - Armed Forces General Hospital",
+        1008 => "Addis Ababa - Bella Defense Referral Hospital",
+        1009 => "Addis Ababa - Besegah Mother and Child Health Hospitals",
+        1010 => "Addis Ababa - Brass Mother and Child Health Hospital",
+        1011 => "Addis Ababa - Dagmawi Minilik Hospital",
+        1012 => "Addis Ababa - DBalch Hospital",
+        1013 => "Addis Ababa - Dinberua Hospital",
+        1014 => "Addis Ababa - Ethio Tebib Hospital",
+        1015 => "Addis Ababa - Federal Police Referral Hospital",
+        1016 => "Addis Ababa - Girum General Hospital",
+        1017 => "Addis Ababa - Hawassa Alatyon Hospital",
+        1018 => "Addis Ababa - Hayat Hospital",
+        1019 => "Addis Ababa - ICMC General Hospital CMC",
+        1020 => "Addis Ababa - Kadisco General Hospital [3]",
+        1021 => "Addis Ababa - Landmark General Hospital",
+        1022 => "Addis Ababa - MyungSung Christian Medical Centre/Korean Hospital/MCM General Hospital",
+        1023 => "Addis Ababa - Nordic Medical Center",
+        1024 => "Addis Ababa - Novocare American Clinic [4]",
+        1025 => "Addis Ababa - Ras Desta Damitew Hospital",
+        1026 => "Addis Ababa - Saint Gabriel General Hospital[5]",
+        1027 => "Addis Ababa - Shedeho Higher Clinic Dr Belaynew Mogess",
+        1028 => "Addis Ababa - St. Paulos Hospital",
+        1029 => "Addis Ababa - St. Yared General Hospital",
+        1030 => "Addis Ababa - Tibebu Hospital",
+        1031 => "Addis Ababa - Tikur Anbesa Specialized Hospital(TASH)",
+        1032 => "Addis Ababa - Tirunesh Beijing General Hospital",
+        1033 => "Addis Ababa - Tzna General Hospital",
+        1034 => "Addis Ababa - Yearrer hospital",
+        1035 => "Addis Ababa - Zenbaba General Hospital",
+        1037 => "Adama - Sr. Aklesia Memorial Hospital",
+        1038 => "Adama - Adama General Hospital & Medical College",
+        1039 => "Adama - Haile Mariam Hospital & Medical College",
+        1040 => "Bahir Dar - Bahir Dar Mini-fistula Hospital",
+        1041 => "Bahir Dar - Felge Hiwot Hospital",
+        1042 => "Bahir Dar - Gamby Teaching Hospital Clinic",
+        1043 => "Bahir Dar - Universal Clinic",
+        1044 => "Bahir Dar - Adinas Clinic",
+        1045 => "Desse - Boru Meda Hospital",
+        1046 => "Desse - Dessie Hospital",
+        1047 => "Desse - selam hospital",
+        1048 => "Desse - ethio hospital",
+        1049 => "Desse - bate hospital",
+        1050 => "Desse - Tossa Hospital",
+        1051 => "Gondar - University of Gondar Hospital",
+        1052 => "Gondar - Ibex Hospital",
+        1053 => "Gondar - Harar",
+        1054 => "Gondar - Harar General Hospital",
+        1055 => "Gondar - Hiwot fana specialized University hospital",
+        1056 => "Hawassa - Adare Hospital",
+        1057 => "Hawassa - Alatiyon Hospital",
+        1058 => "Hawassa - Asher Primary Hospital",
+        1059 => "Hawassa - Awassa Referral Hospital",
+        1060 => "Hawassa - Bete Abrham Primary Hospital",
+        1061 => "Hawassa - Kibru Primary Hospital",
+        1062 => "Hawassa - Yanet Internal Medicine Specialized Center",
+        1063 => "Somali Region - Jig-jiga University Referral Hospital",
+        1064 => "Somali Region - Karamara Hospital",
+        1065 => "Somali Region - Dagahbour General Hospital",
+        1066 => "Somali Region - Gode General Hospital",
+        1067 => "Somali Region - Qabri Dahare General Hospital",
+        1068 => "Somali Region - Warder General Hospital",
+        1069 => "Somali Region - Filtu General Hospital",
+        1070 => "Somali Region - Hargelle General Hospital",
+        1071 => "Somali Region - Sitti General Hospital",
+        1072 => "Tigray - Adigrat Hospital",
+        1073 => "Tigray - Ayder Referral Hospital",
+        1074 => "Tigray - Lemelem Karl Hospital",
+        1075 => "Tigray - Mekelle Hospital",
+        1076 => "Tigray - St. Mary Hospital",
+        1077 => "Tigray - Quiha Hospital",
+        1078 => "Tigray - Sihul Hospital",
+        1079 => "Tigray - Abiadi Hospital",
+        1080 => "Tigray - Wukro Hospital",
+        1081 => "Tigray - korem Hospital",
+        1082 => "Tigray - Mearig Hospital/dansha",
+        1083 => "Tigray - Humera Hospital",
+        1084 => "Tigray - Alamata Hospital",
+        1085 => "Tigray - Adwa Hospital",
+        1086 => "Tigray - Aksum University Referral Hospital",
+        1087 => "Wolayta - Dubbo Catholic Hospital",
+        1088 => "Wolayta - Soddo Christhian Hospital",
+        1089 => "Wolayta - Soddo General Hospital",
+        1090 => "Wolayta - Soddo University Hospital",
+        9999 => "Other"
+    ];
