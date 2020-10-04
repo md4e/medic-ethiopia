b29cf43c4bb2f26c1d10aba14f0da3e7cd088c94
dochoex@gmail.com <abiy.terefe@hotmail.com>
2020-10-04 12:23:35 +0200

    add appointn opd

--- production/patient-list-opd-appointment.php
+++ production/patient-list-opd-appointment.php
@@ -44,15 +44,13 @@ include_once "./config.php";
                       <table id="datatable-buttons" class="table table-striped table-bordered" style="width:100%">
                         <thead>
                           <tr>
-                            <th>Name</th>
-                            <th>Age</th>
-                            <th>Phone No.</th>
+                            <th>Paitent Info</th>
                             <th>Appointment date</th>
                             <th>New Appointment date</th>
                           </tr>
                         </thead>
                         <tbody>
-                          <tr>
+                          <!-- <tr>
                             <td><a href="" class="btn btn-info">Name FatherName</a></td>
                             <td>61</td>
                             <td>0911123456</td>
@@ -70,7 +68,46 @@ include_once "./config.php";
                                 </div>
                               </div>
                             </td>
-                          </tr>
+                          </tr> -->
+                          <?php
+                          $triage_list = new MePatientTable('*');
+                          $result = $triage_list->getResultSet();
+                          $result->data_seek(0);
+                          while ($row2 = $result->fetch_object()) {
+                            echo '<tr>';
+                            echo '<td>';
+                            $patient = new MePatientTable($row2->patient_card_number);
+                            echo '<p class="h6 text-primary"><i class="fa fa-user"></i> ' . $patient->getPatientFirstName() . ', Age:' . $patient->getPatientAge() . '</p>';
+                            echo '<strong>Card No.:</strong> ' . $row2->patient_card_number . ' ';
+                            echo '<p>';
+                            echo '<strong>PhoneNr.:</strong> ' . $patient->getPatientPhone() . ' ';
+                            echo '<strong>Wereda.:</strong> ' . $patient->getPatientWereda() . ' ';
+                            echo '<strong>Kebele.:</strong> ' . $patient->getPatientKebele();
+
+                            // echo '<p><strong><a href="./session.php?patient_card_number=' . $row2->patient_card_number .
+                            //   '" type="button" class="btn btn-sm btn-success"><i class="fa fa-arrow-right"></i> Admit</a></strong></p>';
+                            echo '</td>';
+                            echo '<td>' . date("Y-m-d H:i:s", $row2->patient_registered_date) . '</td>';
+                            echo '<td>
+                          <div>
+                            <div class="custom-control custom-checkbox custom-control-inline m-2">
+                              <a href="#" onclick="changeAppointment(\'open\',\'' . $row2->patient_card_number . '\')" name="change-appointment_' . $row2->patient_card_number .
+                              '" id="change-appointment_' . $row2->patient_card_number . '" type="button"
+                              >Change new date</a>
+
+                            </div>
+                            <div class="change-appointment-' . $row2->patient_card_number . '" style="display:none">
+                              <input id="new-date-opd-' . $row2->patient_card_number . '" name="new-date-opd-' . $row2->patient_card_number . '" type="date" class="form-control" required="required">
+                              <textarea id="new-date-remark-' . $row2->patient_card_number . '" name="new-date-remark-' . $row2->patient_card_number . '" placeholder="Write Reason for new change to send to patient" cols="4" rows="3" class="form-control"></textarea>
+                              <a href="#" onclick="changeAppointment(\'close\',\'' . $row2->patient_card_number . '\')" type="button" id="change-appointment-cancel-' . $row2->patient_card_number . '" class="change-appointment-cancel-' . $row2->patient_card_number . ' btn-sm btn-danger">Cancel Change</a>
+                              <a href="#" type="button" id="change-appointment-success-' . $row2->patient_card_number . '" class="change-appointment-success-' . $row2->patient_card_number . ' btn btn-sm btn-success">Save date and notify</a>
+
+                            </div>
+                          </div>
+                        </td>';
+                            echo '</tr>';
+                          }
+                          ?>
                         </tbody>
                       </table>
                     </div>
@@ -108,19 +145,16 @@ include_once "./config.php";
   <script src="../build/js/custom.min.js"></script>
 
   <script>
-    $(document).ready(function() {
-      $("input[name=change-appointment_0]").on("change", function() {
-        if (this.checked) {
-          $(".change-appointment").show();
-        } else {
-          $("#new-date-opd").val("");
-          $("#new-date-remark").val("");
-          $(".change-appointment").hide();
-
-        }
-        //$("#" + test).show();
-      });
-    });
+    function changeAppointment(param1, param2) {
+      if (param1 == 'open') {
+        $(".change-appointment-" + param2).show();
+      }
+      else if(param1 == 'close') {
+        $("#new-date-opd-" + param2).val("");
+        $("#new-date-remark-" + param2).val("");
+        $(".change-appointment-" + param2).hide();
+      }
+    }
   </script>
 </body>
 
